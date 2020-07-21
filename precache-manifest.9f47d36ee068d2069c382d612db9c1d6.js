f299b5c154214b4bf9a2c48acabba932465b7e16
nihelboujelben <nihelboujelben@gmail.com>
2020-07-21 10:07:33 +0100

    Updates

---
+++ precache-manifest.9f47d36ee068d2069c382d612db9c1d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8260173000c1bb187ddae147971b8b15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b24b7ef0b86f326378d",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "063d928da823e0d81318",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b24b7ef0b86f326378d",
+    "url": "/static/js/2.8992297d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8992297d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063d928da823e0d81318",
+    "url": "/static/js/main.c90a784e.chunk.js"
+  },
+  {
+    "revision": "1ed5b53a246752becdd8",
+    "url": "/static/js/runtime-main.aac46075.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
